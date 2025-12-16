9bec635d63c72df0ebddcd40aee9abfd8b3f0182
ChoiJinong <cjw4767@naver.com>
2025-12-16 16:53:18 +0900

    [Feat] 이동 구현 및 씬 전환
    공 이동 로직 구현
    충돌 및 게임 종료 구현
    로비 -> 스테이지 씬 전환

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 1f;
+    public float jumpForce = 1f;
+    public GameObject panel;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); // -1 ~ 1
+        rb.linearVelocityX = h * moveSpeed;
+        if (transform.position.y < -5)
+        {
+            rb.linearVelocity = Vector2.zero;
+            transform.position = new Vector3(-7.5f, 1, 0);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            transform.position = new Vector3(-7.5f, 1, 0);
+        }
+        else if (collision.gameObject.CompareTag("Finish"))
+        {
+            panel.SetActive(true);
+            Time.timeScale = 0; // ½Ã°£ ¸ØÃã
+        }
+        else
+        {
+            rb.AddForceY(1 * jumpForce);
+        }
+
+    }
+}
